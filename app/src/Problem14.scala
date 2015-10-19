810cf8053baa4c028a112a4355135a4c4b990b31
Radim Pavlicek <radim.pavlicek@gmail.com>
2015-10-19 10:54:40 +0200

    p14
    p14

--- app/src/Problem14.scala
+++ app/src/Problem14.scala
@@ -1,48 +1,42 @@
 
-
-/**
-
- */
-
-import scala.collection.mutable
-import scala.collection.mutable.MutableList
 object Problem14 {
 
-  var max = 0
-  var maxi = 1
+  var max :Long= 0
+  var maxi: Long = 1
 
-  def update(list: mutable.MutableList[Long]) = {
-    for (i<-list.indices)
+
+  def process(k: Long): Long = {
+//    Console.out.println(k)
+    val maybeLong: Option[Long] = z.get(k)
+    if (maybeLong.isEmpty) {
+      val o: Long = relax(k)
+      z(k) = 1 + process(o)
+      z(k)
+    } else {
+      maybeLong.get
+    }
+
   }
 
-  def process(k: Int) = {
-    var i = k
-    var count = 0
-    var list = MutableList[Long]()
-    do {
-        list += i.toLong
-        i = relax(i)
-        count = count + 1
-    } while (i != 1)
-    update(list)
-  }
-  def relax(i: Int):Int = {
+  def relax(i: Long): Long = {
     if (i % 2 == 0)
-       i /2
+      i / 2
     else
       3 * i + 1
   }
 
   val limit = 1000000
-  val z = new Array[Long](limit)
-  for (j <- z.indices)
-    z(j) = 0
+  val z = collection.mutable.Map[Long, Long]()
+  z(1) = 1
 
   def main(args: Array[String]): Unit = {
     var i = 2
     while (i < 1000000) {
-      Console.out.println(i)
-      process(i)
+      val t = process(i)
+      if (t > max) {
+        max = t
+        maxi = i
+      }
       i = i + 1
     }
     Console.out.println(maxi)
